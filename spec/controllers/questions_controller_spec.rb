c4985a65b1ac4f93e0cc1347294a39527b102574
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-12 17:10:46 -0800

    adds RSpec for question model

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe QuestionsController, :type => :controller do
     end
   end
 
+  describe "GET questions" do
+    it "returns http success" do
+      get :questions
+      expect(response).to have_http_status(:success)
+    end
+
+    it "lists all questions on the page" do
+      get :questions
+      expect(last_response.body).to include("hey there!")
+    end
+  end
+
 end
