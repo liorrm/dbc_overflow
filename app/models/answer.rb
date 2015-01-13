b14771b8e60d7ead43e83cd0ca3d5b9c747ba118
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-13 10:57:00 -0800

    adds validations to question and answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,4 @@
 class Answer < ActiveRecord::Base
+  validates_presence_of :title, :content
   belongs_to :question
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
+  validates_presence_of :title, :content
   has_many :answers
 end
