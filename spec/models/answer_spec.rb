bb2fed02210679ab64483a44f48818885f89fcae
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-12 17:11:14 -0800

    adds RSpec for answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Answer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    describe "Answer model" do
+
+    it "creates a new question" do
+      expect { Answer.create(title: "You are a bad programmer", content: "please be a barista isntead")}.to change { Answer.count }.by(1)
+    end
+
+    it "creates a new question with given params" do
+      question = Answer.create(title: "Hey there", content: "whats going on?")
+      expect(question.title).to eql("Hey there")
+    end
+
+  end
+
 end
