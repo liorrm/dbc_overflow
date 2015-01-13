51bac0fd4f3f7a4b2446fbd72be6588eaf7ff18c
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-12 17:11:29 -0800

    updates question model Rspec tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Question model" do
+
+    it "creates a new question" do
+      expect { Question.create(title: "Hey there", content: "whats going on?")}.to change { Question.count }.by(1)
+    end
+
+    it "creates a new question with given params" do
+      question = Question.create(title: "Hey there", content: "whats going on?")
+      expect(question.title).to eql("Hey there")
+    end
+
+  end
+
 end
