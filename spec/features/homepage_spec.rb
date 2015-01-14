b2284983e4d14e51adcc8ed10d674654a7cfea3f
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-14 09:26:55 -0800

    adds another capybara feature test to suite

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,13 +7,21 @@ describe "Homepage", :js => true do ## js true allows javascript
     expect(page).to have_content(question.title)
   end
 
+  it "user can click on create question link and be redirected"
+
   it "user can create a question" do
-    later = FactoryGirl.attributes_for :question
-    # just implement these later, chamud. your situation is a little different from the lecture.
-    #you can use id, name, or placeholder for Capybara forms. otherwise it wont find them.
+    question = FactoryGirl.attributes_for :question
+    visit '/questions/new'
+    fill_in "question_title", with: question[:title]
+    fill_in "question_content", with: question[:content]
+    click_on "Create Question"
+    expect(page).to have_content question[:title]
   end
 
 end
+
+# just implement these later, chamud. your situation is a little different from the lecture.
+    #you can use id, name, or placeholder for Capybara forms. otherwise it wont find them.
 
 
 # by default, let "lazy loads", which means it doesnt store into database until it is used. But this is a problem because by the time we need it we are on the page. To mitigate this, just add bang to let on line 4, which "eager loads" it.
