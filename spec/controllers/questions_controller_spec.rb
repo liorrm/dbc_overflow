b0ef9aa9af01d94a51414f2c2922a46d1d3569a6
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-13 18:48:21 -0800

    adds much improved controller testing suite using factory girl

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
 
-  describe "GET index" do
+  context "GET #index" do
 
     it "returns http success" do
       get :index
@@ -15,7 +15,33 @@ RSpec.describe QuestionsController, :type => :controller do
       get :index
       expect(assigns(:questions)).to eq Question.all # assigns method is rails-specific
     end
-
-
   end
+
+  context "POST #create" do
+
+  it "creates a question if params are valid" do
+    question_params = FactoryGirl.attributes_for :question
+    expect {
+      post :create, :question => question_params
+    }.to change { Question.count }.by(1)
+  end
+
+  it "doesn't create a question if params are invalid" do
+    question_params = FactoryGirl.attributes_for :question
+    question_params.delete(:title)
+    expect {
+      post :create, :question => question_params
+    }.to_not change { Question.count }
+  end
+
+  it "redirects to root if successful" do
+    question_params = FactoryGirl.attributes_for :question
+    expect {
+      post :create, :question => question_params
+    }.to change { Question.count }.by(1)
+  end
+
+end
+
+
 end
