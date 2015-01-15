67d8d39c2881d6df249575d77f76dd00550b64c7
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-15 11:04:43 -0800

    adds line that will destroy associated votes when question/answer deleted

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   validates_presence_of :title, :content
   has_many :answers
-  has_many :votes, as: :votable
+  has_many :votes, as: :votable, dependent: :destroy
 end
