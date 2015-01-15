e4de842e935a4b4618ab643aabf2f0282cf52005
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-14 16:34:38 -0800

    updates schema with latest tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114174113) do
+ActiveRecord::Schema.define(version: 20150114235244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20150114174113) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "quotes", force: true do |t|
+    t.string   "quotes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "votes", force: true do |t|
