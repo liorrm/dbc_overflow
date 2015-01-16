94664a989a441a17a6d3d896ecc33905db98fb78
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-15 16:37:59 -0800

    adds helper method to get vote total

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,11 @@ class Question < ActiveRecord::Base
   validates_presence_of :title, :content
   has_many :answers
   has_many :votes, as: :votable, dependent: :destroy
+
+  def total_votes
+    upvotes = self.votes.where(direction: "up").count
+    downvotes = self.votes.where(direction: "down").count
+    total_votes = upvotes - downvotes
+    return total_votes
+  end
 end
